e05794aaa87d5adb124cc480391eeb92a8992d74
KagariSan <vinhcht@gmail.com>
2023-02-20 18:53:48 +0700

    Chua bai

--- src/de1/Question.java
+++ src/de1/Question.java
@@ -22,10 +22,33 @@ public class Question {
         System.out.println(students.length);
 
         // Tim va in ten nguoi co diem so cao nhat
+        int viTriCuaHSDiemSoCAoNhat = 0;
+        for (int i = 1; i < students.length; i++) {
+            int diemSoCaoNhatHIenTai = students[viTriCuaHSDiemSoCAoNhat].mark;
+
+            int diemSoHocSinhHienTai = students[i].mark;
+
+            if (diemSoCaoNhatHIenTai < diemSoHocSinhHienTai) {
+                viTriCuaHSDiemSoCAoNhat = i;
+            }
+        }
+        System.out.println("Hoc Sinh Co Diem Cao Nhat: " + students[viTriCuaHSDiemSoCAoNhat].name);
 
         // In ra ten cua nguoi co diem so thap nhat
 
         // In ra lan luot ten va diem so + xep hang cuar tung HS
         // 0-6 tb, 6-8 kha, 8-10 gioi
+        for (Student hs: students) {
+            System.out.println("Ten: " + hs.name);
+            System.out.println("Diem so: " + hs.mark);
+
+            if (hs.mark > 8) {
+                System.out.println("Xep hang Gioi");
+            } else if (hs.mark > 6) {
+                System.out.println("Xep hang Kha");
+            } else {
+                System.out.println("Xep hang TB");
+            }
+        }
     }
 }
